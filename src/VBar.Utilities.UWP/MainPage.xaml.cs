d2c957a1f81b4275989e0c463fff30bcb19c5ee1
Richard van Aken <richard@promotigo.com>
2022-10-06 15:12:05 +0100

    stage

---
+++ src/VBar.Utilities.UWP/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace VBarUtilities.UWP
+{
+    using Models;
+
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            VBarUtilities.App.ClassicImport = new ClassicImportUwp();
+
+            VBarUtilities.App.Screenshots = new Screenshots();
+
+            VBarUtilities.App.Platform = new Platform();
+
+            LoadApplication(new VBarUtilities.App());
+        }
+    }
+}
